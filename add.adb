fd7dc2507df1ea2c0481f9e4846369b699aa5190
Daniel Garcia <daniescorpio@hotmail.es>
2020-10-14 12:03:34 +0200

    tarea de giro de volante

--- add.adb
+++ add.adb
@@ -25,8 +25,12 @@ package body add is
     -----------------------------------------------------------------------
     ------------- constants 
     -----------------------------------------------------------------------
-    LEER_POSICION_CABEZA_PRIORITY : Integer := 9;
-    LEER_DISTANCIA_PRIORITY 	  : Integer := 8;
+    LEER_POSICION_CABEZA_PRIORITY : Integer := 1;
+    LEER_GIRO_VOLANTE_PRIORITY 	  : Integer := 2;
+    LEER_DISTANCIA_PRIORITY 	  : Integer := 3;
+    
+    INTERVALO_LECTURA_POSICION_CABEZA : Time_Span := Milliseconds (400);
+    INTERVALO_LECTURA_GIRO_VOLANTE    : Time_Span := Milliseconds (350);
 
     -----------------------------------------------------------------------
     ------------- declaration of tasks 
@@ -41,6 +45,10 @@ package body add is
     task leerPosicionCabeza is 
       pragma priority (LEER_POSICION_CABEZA_PRIORITY);
     end leerPosicionCabeza;
+    
+    task leerGiroVolante is
+      pragma priority (LEER_GIRO_VOLANTE_PRIORITY);
+    end leerGiroVolante;
 
     -----------------------------------------------------------------------
     ------------- body of tasks 
@@ -49,8 +57,8 @@ package body add is
     -- Aqui se escriben los cuerpos de las tareas 
 
     --task body leerDistancia20_200ms is
-      --Current_D: Distance_Samples_Type := 0;
-      --Current_V: Speed_Samples_Type := 0;
+      --Current_D : Distance_Samples_Type := 0;
+      --Current_V : Speed_Samples_Type := 0;
     
     --begin
       --Starting_Notice ("Prueba sensor distancia");
@@ -80,18 +88,26 @@ package body add is
     --end leerDistancia20_200ms;
     
     
+    -----------------------------------------------------------------------
+    ------------- TAREA POSICION CABEZA 
+    -----------------------------------------------------------------------
+    
     task body leerPosicionCabeza is 
+    
       Current_H   	 : HeadPosition_Samples_Type;
       Previous_H	 : HeadPosition_Samples_Type := (0, 0);
       Siguiente_Instante : Time;
-      Intervalo	 	 : Time_Span := Milliseconds (400);
-    begin
       
-      Siguiente_instante := Clock + Intervalo;
+    begin
+    
+      Siguiente_instante := Clock + INTERVALO_LECTURA_POSICION_CABEZA;
       Starting_Notice ("Prueba sensor cabeza");
+      
       loop
+      
         Reading_HeadPosition (Current_H);
         Display_HeadPosition_Sample (Current_H);
+        
         if (Current_H(x) > 30 OR Current_H(x) < -30) then 
           if (Previous_H(x) > 30 OR Previous_H(x) < -30) then
             --Sintoma de somnolencia o distraccion
@@ -107,12 +123,58 @@ package body add is
             New_Line;
           end if;
         end if;
+        
         Previous_H := Current_H;
+        
         delay until Siguiente_Instante;
-        Siguiente_Instante := Siguiente_Instante + Intervalo;
+        Siguiente_Instante := Siguiente_Instante + INTERVALO_LECTURA_POSICION_CABEZA;
+        
       end loop;
+      
       Finishing_Notice ("Prueba sensor cabeza");
+      
     end leerPosicionCabeza;
+    
+    
+    -----------------------------------------------------------------------
+    ------------- TAREA GIRO VOLANTE 
+    -----------------------------------------------------------------------
+    
+    task body leerGiroVolante is
+    
+      Current_S     	 : Steering_Samples_Type;
+      Previous_S    	 : Steering_Samples_Type := 0;
+      Actual_Offset 	 : Steering_Samples_Type;
+      Siguiente_Instante : Time;
+      
+    begin
+      
+      Siguiente_instante := Clock + INTERVALO_LECTURA_GIRO_VOLANTE;
+      Starting_Notice ("Prueba sensor giro volante");
+      
+      loop
+        
+        Reading_Steering (Current_S);
+        Display_Steering (Current_S);
+        
+        Actual_Offset := Current_S - Previous_S;
+        
+        if (Actual_Offset > 20 OR Actual_Offset < -20) then 
+          --Sintoma de volantazo
+          Put (" --> VOLANTAZO");
+          New_Line;
+        end if;
+        
+        Previous_S := Current_S;
+        
+        delay until Siguiente_Instante;
+        Siguiente_Instante := Siguiente_Instante + INTERVALO_LECTURA_GIRO_VOLANTE;
+        
+      end loop;
+      
+      Finishing_Notice ("Prueba sensor giro volante");
+      
+    end leerGiroVolante;
    
     ----------------------------------------------------------------------
     ------------- procedure para probar los dispositivos 
@@ -120,12 +182,12 @@ package body add is
     procedure Prueba_Dispositivos; 
 
     Procedure Prueba_Dispositivos is
-        Current_V: Speed_Samples_Type := 0;
-        Current_H: HeadPosition_Samples_Type := (+2,-2);
-        Current_D: Distance_Samples_Type := 0;
-        Current_O: Eyes_Samples_Type := (70,70);
-        Current_E: EEG_Samples_Type := (1,1,1,1,1,1,1,1,1,1);
-        Current_S: Steering_Samples_Type := 0;
+        Current_V : Speed_Samples_Type := 0;
+        Current_H : HeadPosition_Samples_Type := (+2,-2);
+        Current_D : Distance_Samples_Type := 0;
+        Current_O : Eyes_Samples_Type := (70,70);
+        Current_E : EEG_Samples_Type := (1,1,1,1,1,1,1,1,1,1);
+        Current_S : Steering_Samples_Type := 0;
     begin
          --Starting_Notice ("Prueba_Dispositivo");
 

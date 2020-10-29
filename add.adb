13ac9064b13296b9b764a04a6d75cca77e46e4ef
Daniel Garcia <daniescorpio@hotmail.es>
2020-10-29 17:40:25 +0100

    Measurement of computing time tasks

--- add.adb
+++ add.adb
@@ -26,11 +26,12 @@ package body add is
     ------------- constants 
     -----------------------------------------------------------------------
     
-    LEER_POSICION_CABEZA_PRIORITY 	  : Integer   := 1;
-    LEER_DISTANCIA_PRIORITY 	  	  : Integer   := 2;
+    MOSTRAR_INFO_DISPLAY_PRIORITY  	  : Integer   := 1;
+    LEER_POSICION_CABEZA_PRIORITY 	  : Integer   := 2;
     LEER_GIRO_VOLANTE_PRIORITY 	  	  : Integer   := 3;
-    CALCULAR_RIESGOS_PRIORITY	  	  : Integer   := 4;
-    MOSTRAR_INFO_DISPLAY_PRIORITY  	  : Integer   := 5;
+    LEER_DISTANCIA_PRIORITY 	  	  : Integer   := 4;
+    CALCULAR_RIESGOS_PRIORITY	  	  : Integer   := 5;
+    
     INTERVALO_DETECCION_RIESGOS		  : Time_Span := Milliseconds (150);
     INTERVALO_LECTURA_DISTANCIA_SEGURIDAD : Time_Span := Milliseconds (300);
     INTERVALO_LECTURA_GIRO_VOLANTE        : Time_Span := Milliseconds (350);
@@ -149,10 +150,12 @@ package body add is
         Sintomas.Guardar_Estado_Distraccion(Distraccion);
         Distraccion := CABEZA_NO_INCLINADA;
         
+        Finishing_Notice ("Tarea lectura de cabeceo finalizada.");
+        
         delay until Siguiente_Instante;
         Siguiente_Instante := Siguiente_Instante + INTERVALO_LECTURA_POSICION_CABEZA;
         
-        Finishing_Notice ("Tarea lectura de cabeceo finalizada.");
+        
         
       end loop;
       
@@ -197,11 +200,13 @@ package body add is
 	Medidas.Guardar_Velocidad_Actual(Current_V);
 	Sintomas.Guardar_Estado_Distancia(Distancia_Sintoma);
 	Distancia_Sintoma := DISTANCIA_SEGURA;	
+	
+        Finishing_Notice ("Tarea lectura distancia de seguridad finalizada.");
 
 	delay until Siguiente_Instante;
         Siguiente_Instante := Siguiente_Instante + INTERVALO_LECTURA_DISTANCIA_SEGURIDAD;
         
-    	Finishing_Notice ("Tarea lectura distancia de seguridad finalizada.");
+    	
     	
         end loop;
       
@@ -244,10 +249,12 @@ package body add is
         Sintomas.Guardar_Estado_Volantazo(Volantazo);
         Volantazo := ESTADO_NO_VOLANTAZO;
         
+        Finishing_Notice ("Tarea lectura datos volante finalizada.");
+        
         delay until Siguiente_Instante;
         Siguiente_Instante := Siguiente_Instante + INTERVALO_LECTURA_GIRO_VOLANTE;
               
-        Finishing_Notice ("Tarea lectura datos volante finalizada.");
+        
         
       end loop;
       
@@ -318,11 +325,13 @@ package body add is
 	put ("-----------------------------------------------------");
 	New_Line;
 	New_Line;
+	
+        Finishing_Notice ("Tarea mostrar informacion en display finalizada.");
         
         delay until Siguiente_Instante;
         Siguiente_Instante := Siguiente_Instante + INTERVALO_REFRESCO_DISPLAY;
         
-        Finishing_Notice ("Tarea mostrar informacion en display finalizada.");
+        
         
       end loop;
       
@@ -379,11 +388,11 @@ package body add is
 	  Beep(5);
 	  Activate_Brake;
 	end if;
+	
+	Finishing_Notice ("Tarea deteccion de riesgos finalizada.");
 
         delay until Siguiente_Instante;
         Siguiente_Instante := Siguiente_Instante + INTERVALO_DETECCION_RIESGOS;
-        
-        Finishing_Notice ("Tarea deteccion de riesgos finalizada.");
         
       end loop;
       
@@ -451,69 +460,10 @@ package body add is
         return VELOCIDAD;
       end Obtener_Velocidad_Actual;
       
-      
     end Medidas;
     
-   
-    ----------------------------------------------------------------------
-    ------------- procedure para probar los dispositivos 
-    ----------------------------------------------------------------------
-    procedure Prueba_Dispositivos; 
-
-    Procedure Prueba_Dispositivos is
-        Current_V : Speed_Samples_Type := 0;
-        Current_H : HeadPosition_Samples_Type := (+2,-2);
-        Current_D : Distance_Samples_Type := 0;
-        Current_O : Eyes_Samples_Type := (70,70);
-        Current_E : EEG_Samples_Type := (1,1,1,1,1,1,1,1,1,1);
-        Current_S : Steering_Samples_Type := 0;
-    begin
-         --Starting_Notice ("Prueba_Dispositivo");
-
-         --for I in 1..120 loop
-         -- Prueba distancia
-            --Reading_Distance (Current_D);
-            --Display_Distance (Current_D);
-            --if (Current_D < 40) then Light (On); 
-            --                    else Light (Off); end if;
-
-         -- Prueba velocidad
-            --Reading_Speed (Current_V);
-            --Display_Speed (Current_V);
-            --if (Current_V > 110) then Beep (2); end if;
-
-         -- Prueba volante
-            --Reading_Steering (Current_S);
-            --Display_Steering (Current_S);
-            --if (Current_S > 30) OR (Current_S < -30) then Light (On);
-            --                                         else Light (Off); end if;
-
-         -- Prueba Posicion de la cabeza
-            --Reading_HeadPosition (Current_H);
-            --Display_HeadPosition_Sample (Current_H);
-            --if (Current_H(x) > 30) then Beep (4); end if;
-
-         -- Prueba ojos
-            --Reading_EyesImage (Current_O);
-            --Display_Eyes_Sample (Current_O);
-
-         -- Prueba electroencefalograma
-            --Reading_Sensors (Current_E);
-            --Display_Electrodes_Sample (Current_E);
-   
-         --delay until (Clock + To_time_Span(0.1));
-         --end loop;
-
-         --Finishing_Notice ("Prueba_Dispositivo");
-         null;
-    end Prueba_Dispositivos;
-
-
 begin
    null;
-   --Starting_Notice ("Programa Principal");
-   --Prueba_Dispositivos;
-   --Finishing_Notice ("Programa Principal");
 end add;
 
 
